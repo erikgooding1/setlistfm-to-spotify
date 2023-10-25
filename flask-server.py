7732953b1782ca47d73cf4ff5544d93db575dbdc
erikgooding1 <erik.gooding1@gmail.com>
2023-10-25 14:51:35 -0400

    Added some logic for warning messages on playist creation cancelling

--- flask-server.py
+++ flask-server.py
@@ -19,10 +19,15 @@ def index():
 def execute():
     url = request.form['url']
 
+    
     playlist_link = subprocess.run(['python', 'generate-setlist.py', url], stdout=subprocess.PIPE, universal_newlines=True)
-    embed_code = f'<iframe src="https://open.spotify.com/embed/playlist/{playlist_link.stdout.split(":")[2]}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'
+    if playlist_link.stdout == "Playlist creation cancelled\n":
+        warning = f'<div id="warning" class="alert alert-warning" role="alert">This is a warning alert—check it out!</div>'
+        return render_template('index.html', warning=warning)
+    else:
+        embed_code = f'<iframe src="https://open.spotify.com/embed/playlist/{playlist_link.stdout.split(":")[2]}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'
     #print(embed_code)
-    return render_template('output.html', embed_code=embed_code)
+        return render_template('output.html', embed_code=embed_code)
 
 if __name__ == '__main__':
     app.run(debug=True)
